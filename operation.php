db8741183ec1ee4ded3663ceae30182f417ca10e
nivethitha2312 <nivethitha2312@gmail.com>
2017-10-09 03:34:06 +0530

    Update operation.php

--- operation.php
+++ operation.php
@@ -10,10 +10,7 @@ $database = new medoo([
 	'password' => '7e67552bb2c70f0a4ab752a6ef9764efbf286390604af0439a898758eef80c9a',
 	'charset' => 'utf8',
 	]);
-
-	
-$status = $database->query("insert into salesforce.onboardformmodel__c (accountNumber, name, phone) values('".$_POST['advCompName']."','".$_POST['advFname']."','".$_POST['advLname']."','".$_POST['phone']."')");
-
+$status = $database->query("insert into salesforce.onboardformmodel__c (advertiser_company_name__c, advertiser_contact_first_name__c, advertiser_contact_last_name__c, advertiser_contact_phone__c, advertiser_web_address__c, advertiser_email_address__c, business_description__c, location_name__c, address_1__c, city__c, postal_code__c, phone__c) values('".$_POST['advCompName']."','".$_POST['advFname']."','".$_POST['advLname']."','".$_POST['advContactPhone']."','".$_POST['advWebAddr']."','".$_POST['advEmailAddr']."','".$_POST['businessDesc']."','".$_POST['locationName']."','".$_POST['addr1']."','".$_POST['city']."','".$_POST['postalCode']."','".$_POST['phone']."')");
 if($database->error()[2]) {
 	echo $database->error()[2];
 } else {
