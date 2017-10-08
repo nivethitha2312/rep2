29d8782d54cb8252331ebf5c3db8578199b89d38
nivethitha2312 <nivethitha2312@gmail.com>
2017-10-09 00:54:45 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -2,11 +2,9 @@
 echo "<pre>";
 require 'vendor/medoo.php';
 
-$myPDO = new PDO('pgsql:host=ec2-54-235-123-159.compute-1.amazonaws.com;dbname=d7eorcto5pmnv6', 'famrewhhsamcyp', 'c3a491725143c036350052fafd79aeb42571e2890c437894ce1be6f851995205');
+$database = new medoo("host=ec2-54-235-123-159.compute-1.amazonaws.com dbname=d7eorcto5pmnv6 user=famrewhhsamcyp password=c3a491725143c036350052fafd79aeb42571e2890c437894ce1be6f851995205");
 
- 
 $status = $database->query("insert into salesforce.Account (AccountNumber, Name, Phone) values('".$_POST['AccNo']."','".$_POST['Name']."','".$_POST['phone']."')");
-
 if($database->error()[2]) {
 echo $database->error()[2];
 } else {
