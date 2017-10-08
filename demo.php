088381ed65e70a7c323d118e2bc68fff2e3ce415
nivethitha2312 <nivethitha2312@gmail.com>
2017-10-09 01:51:42 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,9 +1,3 @@
 <?php
-echo "<pre>";
-require 'vendor/medoo.php';
-$status = $database->query("insert into salesforce.Account (AccountNumber, Name, Phone) values('".$_POST['AccNo']."','".$_POST['Name']."','".$_POST['phone']."')");
-if($database->error()[2]) {
-echo $database->error()[2];
-} else {
 echo "Success!!";
-}
+?>
