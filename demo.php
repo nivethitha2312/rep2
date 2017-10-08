4aa89cc58e912b4bc54f0adf2a3711f70570ac61
nivethitha2312 <nivethitha2312@gmail.com>
2017-10-09 00:26:29 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -4,11 +4,12 @@ require 'vendor/medoo.php';
 
 $database = new medoo([
 'database_type' => 'pgsql',
-'database_name' => 'd7eorcto5pmnv6',
-'server' => 'ec2-54-235-123-159.compute-1.amazonaws.com',
-'username' => 'famrewhhsamcyp',
+'Database' => 'd7eorcto5pmnv6',
+'Host' => 'ec2-54-235-123-159.compute-1.amazonaws.com',
+'User' => 'famrewhhsamcyp',
 'password' => 'c3a491725143c036350052fafd79aeb42571e2890c437894ce1be6f851995205',
 'charset' => 'utf8',
+'Port' => '5432',
 ]);
 
  
